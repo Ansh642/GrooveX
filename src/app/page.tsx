b34aac41bace491242e181c88cb55bef972f57c6
Ansh Agarwal <122566057+Ansh642@users.noreply.github.com>
2024-04-09 23:31:05 +0530

    feat : Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
-    <div className="">
-      Hello
-    </div>
+    <main className="min-h-screen bg-black/[.96] antialiased bg-grid-white/[0.02]">
+      
+      <HeroSection/>
+      <FeaturedCourses/>
+    </main>
   );
 }
